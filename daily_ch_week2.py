2885cb202815132849c620c73fa98f35cc5f7139
Iga <igakrsz@gmail.com>
2026-02-22 14:47:55 +0200

    Create daily_ch_week2.py

---
+++ daily_ch_week2.py
@@ -0,0 +1,29 @@
+class Farm:
+    def __init__(self, farm_name):
+        self.name = farm_name
+        self.animals = {}
+
+    def add_animal(self, animal_type, count=1):
+        if animal_type in self.animals:
+            self.animals[animal_type] += count
+        else:
+            self.animals[animal_type] = count
+
+    def get_info(self):
+        output = f"{self.name}'s farm\n\n"
+
+        # loop through animals
+        for animal, amount in self.animals.items():
+            output += f"{animal} : {amount}\n"
+
+        output += "\n\tE-I-E-I-0!"
+        return output
+
+
+macdonald = Farm("McDonald")
+macdonald.add_animal('cow', 5)
+macdonald.add_animal('sheep')
+macdonald.add_animal('sheep')
+macdonald.add_animal('goat', 12)
+
+print(macdonald.get_info())
